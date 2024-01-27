7ca6edcc1d86dc395232845e6153ebecbcd12575
AnshKumar24 <anshkrana9@gmail>
2024-01-27 22:50:17 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,47 @@
+// console.log("I am ansh");
+// console.log("EN21CS301117");
+// age = 24;
+// age + 1;
+
+
+// let name = "ansh kumar"
+// age = 24
+// price = 99;
+// x = null
+// y = undefined
+// isfollow = "true"
+// console.log(name)
+
+
+// let age = 25
+// age = 21
+// age = 20
+// console.log(age)
+
+// const pi = 3.14
+// console.log(pi);
+
+// const student = {
+//     Fullname: "ansh",
+//     age: 21,
+//     cgpa: 8.2,
+//     year: 2024,
+//     isPass: "true"
+//     }
+
+// student[Fullname] = "Ansh Kumar";
+//     console.log(student["Fullname"]);
+//      //or
+//      console.log(student.Fullname);
+
+
+// Que1. create a const object called"product" to store information as shown in picture.
+
+// const product = {
+//     name: "Parker Jotter Standard CT Ball Pen",
+//     color: "black",
+//     price: 270,
+//     rating: 4,
+// }
+
+// console.log(product);
---
+++ test1.js
@@ -0,0 +1,14 @@
+//  let, const and var
+
+const accoundId = 2101162
+let accountemail = "ansh@gmail.com"
+var accountpass = "12345"
+accountcity = "indore"
+
+// let accountstate;
+
+console.log(accoundId);
+console.table([accountemail, accountpass, accountcity]);
+
+// datatypes
+
